c9c719954482f29e351b20e6dc9d0fb093fa1073
Armand <thillard.armand@gmail.com>
2021-06-08 09:30:27 +0200

    initial commit

---
+++ src/main/java/projet/AdventureGaragiste/AdventureGaragisteApplication.java
@@ -0,0 +1,13 @@
+package projet.AdventureGaragiste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureGaragisteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventureGaragisteApplication.class, args);
+	}
+
+}
